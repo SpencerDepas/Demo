7456d4749d3b4b24f8d66bf94ea3aa5eddad246e
SD <spencerdepas@gmail.com>
2014-05-02 19:13:13 -0400

    Random old program

---
+++ CraclePop.java
@@ -0,0 +1,29 @@
+
+public class CraclePop
+	{
+
+		public static void main(String[] args)
+			{
+
+				for(int i = 1; i <= 100; i++){
+					
+					if(i % 3 == 0 && i % 5 == 0){
+						
+						System.out.println("CracklePop");
+						
+					}
+					else if(i % 3 == 0){
+						System.out.println("Cracle");
+					}
+					else if( i % 5 == 0){
+						System.out.println("Pop");
+					}
+					else{
+						System.out.println(i);
+					}
+					
+				}
+				
+
+			}
+	}
